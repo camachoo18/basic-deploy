2d125ce3c9b15db89e3b69c5b22d6d6e78e28d67
camachoo18 <estivencamachops@gmail.com>
2025-03-06 10:38:10 +0100

    update

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const PORT = 3000;
 app.use(express.json());
 
 app.get('/', (req, res) => {
-  res.send('hola mundo mi gente');
+  res.send('hola mundo mi gente update v1');
 });
 
 app.listen(PORT, () => {
